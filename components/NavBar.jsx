b40347fbcccab617340741b427c3fe10d41f0aa9
markbensonhernando <mbhernando27@gmail.com>
2023-11-11 19:38:52 +0800

    Feat: Navbar components and custom css

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,5 +1,23 @@
+import { faHome, faTicket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className='flex justify-between bg-nav p-4'>
+      <div className='flex items-center space-x-4'>
+        <Link href='/'>
+          <FontAwesomeIcon title='Home' icon={faHome} className='icon' />
+        </Link>
+        <Link href='/titlepage/new'>
+          <FontAwesomeIcon title='Tickets' icon={faTicket} className='icon' />
+        </Link>
+      </div>
+      <div>
+        <p className='text-default-text'>mb@gmail.com</p>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
