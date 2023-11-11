b538e455d8d535dd25db4f72675dab9a64be7d88
markbensonhernando <mbhernando27@gmail.com>
2023-11-11 10:58:08 +0800

    Feat: Adding fontawesome. Adding custom tailwindcss color. Adding bgcolor and text color

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,10 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import NavBar from '@/components/NavBar';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -13,8 +17,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <NavBar />
-        {children}
+        <div className='flex flex-col h-screen max-h-screen'>
+          <NavBar />
+          <div className='flex-grow overflow-y-auto bg-page text-default-text'>
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
